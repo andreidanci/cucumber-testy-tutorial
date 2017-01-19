96a1ec2c995f00de1a4f8ffe2eafe1cf5ca84300
Dunch <andrei.danci@gmail.com>
2017-01-19 21:45:33 +0200

    fchangePassword

--- src/test/java/org/fasttrackit/Automation/LoginTest.java
+++ src/test/java/org/fasttrackit/Automation/LoginTest.java
@@ -9,33 +9,74 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class LoginTest extends TestBase{
+public class LoginTest extends TestBase {
 
     @Test
     public void validLoginTest() {
 
+        openBrowser();
+
+        login("eu@fast.com", "eu.pass");
+
+        try {
+
+            WebElement logoutBtn = driver.findElement(By.linkText("Logout"));
+            logoutBtn.click();
+        } catch (NoSuchElementException er) {
+            Assert.fail("Could not login. Logout button was not pressent!");
+
+        }
+
+    }
+
+    private void openBrowser() {
         System.out.println("ready");
         driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+    }
+
+    @Test
+    public void invalidPasswordTest() {
+
+        openBrowser();
+
+        login("eu@fast.com", "eu.pass123");
+
+        WebElement errorElement = driver.findElement(By.className("error-msg"));
+        System.out.println(errorElement.getText());
+        Assert.assertEquals(errorElement.getText(), "Invalid user or password!");
+
+
+    }
+
+    public void login(String user, String pass) {
 
         WebElement emailField = driver.findElement(By.id("email"));
-        WebElement passField =  driver.findElement(By.name("password"));
+        WebElement passField = driver.findElement(By.name("password"));
         WebElement loginBtn = driver.findElement(By.className("login-btn"));
 
-
-        emailField.sendKeys("eu@fast.com");
-        passField.sendKeys("eu.pass111");
+        emailField.sendKeys(user);
+        passField.sendKeys(pass);
         loginBtn.click();
 
-     try {
 
-         WebElement logoutBtn = driver.findElement(By.linkText("Logout"));
-         logoutBtn.click();
-     } catch (NoSuchElementException er) {
-         Assert.fail("Could not login. Logout button was not pressent!");
+    }
 
-     }
+    @Test
+    public void changePasswordWithInvalidCurrentPassword() {
 
+        openBrowser();
+        login("eu@fast.com", "eu.pass");
 
+        WebElement preferenceButton = driver.findElement(By.xpath("//button[@data-target='#preferences-win']"));
+        preferenceButton.click();
+
+        WebElement currentPasswordField = driver.findElement(By.name("password"));
+        WebElement newPasswordField = driver.findElement(By.name("newPassword"));
+        WebElement repeatPasswordField = driver.findElement(By.name("newPasswordRepeat"));
+        repeatPasswordField.sendKeys("new.pass");
+
+        currentPasswordField.sendKeys("wrong.pass");
+        newPasswordField.sendKeys("new.pass");
 
 
     }
